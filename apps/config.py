5bf31d94b7805f5cf857529a16b6782293b8852d
taner <stanermetin@gmail.com>
2022-09-16 10:21:39 +0200

    remove crunchbase links from the pages

--- apps/config.py
+++ apps/config.py
@@ -14,8 +14,8 @@ class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY', 'S3cr3t')
 
     # This will create a file in <app> FOLDER
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://ungbomjbflqjpy:468a9dd86ba9a3fd9e7e0821599fc5bbeba6d45e35db75a21121eeddf97fd005@ec2-44-195-100-240.compute-1.amazonaws.com:5432/ddbfli0ijii842'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://ungbomjbflqjpy:468a9dd86ba9a3fd9e7e0821599fc5bbeba6d45e35db75a21121eeddf97fd005@ec2-44-195-100-240.compute-1.amazonaws.com:5432/ddbfli0ijii842'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
     # Assets Management
@@ -29,15 +29,15 @@ class ProductionConfig(Config):
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_DURATION = 3600
 
-    # # PostgreSQL database
-    # SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
-    #     os.getenv('DB_ENGINE'   , 'mysql'),
-    #     os.getenv('DB_USERNAME' , 'fixhub'),
-    #     os.getenv('DB_PASS'     , 'pass'),
-    #     os.getenv('DB_HOST'     , 'localhost'),
-    #     os.getenv('DB_PORT'     , 3306),
-    #     os.getenv('DB_NAME'     , 'fixhub_db')
-    # ) 
+    # PostgreSQL database
+    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
+        os.getenv('DB_ENGINE'   , 'mysql'),
+        os.getenv('DB_USERNAME' , 'fixhub'),
+        os.getenv('DB_PASS'     , 'pass'),
+        os.getenv('DB_HOST'     , 'localhost'),
+        os.getenv('DB_PORT'     , 3306),
+        os.getenv('DB_NAME'     , 'fixhub_db')
+    ) 
 
 class DebugConfig(Config):
     DEBUG = True
