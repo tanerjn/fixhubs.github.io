638053673b2f029e1f49e0a95596a97236c9ef1c
tanerjn <stanermetin@gmail.com>
2022-11-17 20:07:18 +0100

    update firewall settings

--- nginx/set_ufw.sh
+++ nginx/set_ufw.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
 sudo mkdir /etc/nginx/sites-enabled
 sudo ln -s /etc/nginx/sites-available/fixhub /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl restart nginx
-sudo ufw delete allow 5000
+sudo ufw allow 5000
 sudo ufw allow 'Nginx Full'
 ufw app list
-man ufw
 ufw allow 80,443/tcp
 vi /etc/ufw/applications.d/nginx.ini
 ufw app update nginx
 ufw app info 'Nginx HTTP'
 ufw allow 'Nginx HTTP'
-sudo resolvectl flush-caches
-sudo journalctl -r -u systemd-resolved
+#sudo resolvectl flush-caches
+#sudo journalctl -r -u systemd-resolved
+
+
+nohup uwsgi /var/www/example/config-uwsgini &
