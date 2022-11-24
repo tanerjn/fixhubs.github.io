8a592f6fa3378e819ad1317a68cd259caf966a2d
tanerjn <stanermetin@gmail.com>
2022-11-24 10:20:33 +0100

    update requierements

--- pre_install.sh
+++ pre_install.sh
@@ -35,6 +35,8 @@ sudo cp nginx/app.py /var/www/example
 sudo cp nginx/config-nginx.conf /var/www/example/
 sudo cp nginx/default.conf /etc/nginx/conf.d/ 
 
+python3 -m pip install python-dev-tools --user --upgrade
+
 #certbot --nginx --redirect -d fixhubs.de 
 
 
